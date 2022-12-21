0be4345f1b94c724a786dc8ec3e4883a9764600a
Saqib Ali <docbook.xml@gmail.com>
2022-12-20 16:16:30 -0800

    Update conditional_true_event.sql

--- conditional_true_event.sql
+++ conditional_true_event.sql
@@ -7,3 +7,23 @@ select
       over (partition by symbol order by date) as days_of_increase
 from us_stock_market_data_for_data_science.public.stock_history
 where symbol='SNOW';
+
+
+-- same query without using conditional_true_event
+with increase_flagged as (
+  select 
+    symbol
+    , date
+    , close
+    , close > lag(close) over (partition by symbol order by date) as increase_flag
+    from us_stock_market_data_for_data_science.public.stock_history
+)
+select 
+  symbol
+  , date
+  , close
+  , count_if(increase_flag) over(partition by symbol order by date) as days_of_increase
+from increase_flagged  
+where symbol='SNOW';
+
+
